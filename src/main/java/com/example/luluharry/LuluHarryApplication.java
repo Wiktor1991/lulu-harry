13e8c14e54d852d97798cde0cf89bccd14135d33
elwiktor <lulinskiwiktor@gmail.com>
2023-11-20 11:07:08 +0100

    first commit

---
+++ src/main/java/com/example/luluharry/LuluHarryApplication.java
@@ -0,0 +1,13 @@
+package com.example.luluharry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuluHarryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuluHarryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/luluharry/LuluHarryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.luluharry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuluHarryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
